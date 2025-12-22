413d0ec1ce452d0fa6d66978476b226c81a75949
Ridho Dimas <it.staff.hew@gmail.com>
2025-12-22 10:14:08 +0700

    updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -76,7 +76,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0726576f323a567d0e1c413be161ead5
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "f4ff3dd9fac0e6a77d77361632442e5c",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "2883af3467779af178045f65f671ea90",
+"flutter_bootstrap.js": "27b9c1c09e1c23758337ea1dccb4335b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
